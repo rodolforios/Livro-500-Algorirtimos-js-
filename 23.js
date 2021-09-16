135252081eba68b77ae10665b36ed12d0fd106bf
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2021-09-16 10:18:18 -0300

    Create 23.js

---
+++ 23.js
@@ -0,0 +1,4 @@
+//Entrar com um número inteiro de 3 casas e imprimir o algarismo da casa das dezenas.
+var readlineSync = require ('readline-sync');
+var n1 = readlineSync.question('Digite um número de 3 casas: ')
+console.log(n1*n1)
