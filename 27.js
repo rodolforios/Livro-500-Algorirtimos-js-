1e00c140ab59c24b7d2c3be144d3f03a4b0cefbf
Rodolfo Rios <rodolforios18@gmail.com>
2021-10-05 14:05:48 -0300

    27

---
+++ 27.js
@@ -0,0 +1,14 @@
+//imprimir dois valores e fazer o decremento de um e acrescentar uma unidade a outro
+
+let x = 2;
+let y = 5;
+
+console.log(`O Valor de x é ${x}`);
+x++
+console.log(`O Valor de y é ${y}`);
+y--
+
+console.log(`O novo valore de x é ${x}`)
+
+console.log(`O novo valore de y é ${y}`)
+
