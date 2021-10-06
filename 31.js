c42426f75c3065fcece1f4f159e7bdf188a816d8
Rodolfo Rios <rodolforios18@gmail.com>
2021-10-05 22:17:33 -0300

    31

---
+++ 31.js
@@ -0,0 +1,9 @@
+//Criar um algoritmo que imprima a média aritmética entre os números 8, 9 e 7.
+
+var a = 8;
+var b = 9;
+var c = 7;
+var mediaAritimetica = (a+b+c)/3;
+
+console.log(`A média aritimetica entre ${a},${b} e ${c} é ${mediaAritimetica}`)
+
