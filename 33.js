8e4f44f77a34566ce6438216369cdf8e3a1839bb
Rodolfo Rios <rodolforios18@gmail.com>
2021-10-06 10:26:38 -0300

    33

---
+++ 33.js
@@ -0,0 +1,10 @@
+//Ler dois números inteiros e imprimi-los.
+
+const readline = require('readline-sync');
+
+const num1 = readline.question('Digite um número inteiro: ')
+const num2 = readline.question('Digite outro número inteiro: ')
+
+
+console.log(num1);
+console.log(num2);
