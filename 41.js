d76e28450be58c31ed9ad69f72899f226ce75cc7
Rodolfo Rios <rodolforios18@gmail.com>
2021-10-09 17:40:58 -0300

    correcao

--- 41.js
+++ 41.js
@@ -10,4 +10,3 @@ let num4 = Number(readline.question('Digite um número: ') * 4);
 
 let medPon = (num1 + num2 + num3 + num4) / 10;
 console.log(`A média ponderada é: ${medPon}`)
-console.log('teste2')
